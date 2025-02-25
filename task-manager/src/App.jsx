cdb4dd9855066ec58e8f7f33e795382b286f7ec9
Bina <benjaminbinyam500@gmail.com>
2025-02-25 14:31:09 +0300

    Task manager project setup tailwind added

---
+++ task-manager/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <h1 class="text-3xl bg-green-500 font-bold underline">Hello world!</h1>
+  );
+}
+
+export default App;
